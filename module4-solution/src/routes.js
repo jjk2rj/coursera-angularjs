0ba1e0d3591e028b17397b15e7623bc1e6a816bc
Johnny Kim <jxkim89@gmail.com>
2019-05-01 03:27:15 -0400

    module4 complete

--- module4-solution/src/routes.js
+++ module4-solution/src/routes.js
@@ -7,24 +7,35 @@ angular.module('MenuApp')
 RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 function RoutesConfig($stateProvider, $urlRouterProvider) {
 
-  // Redirect to home page if no other URL matches
   $urlRouterProvider.otherwise('/');
 
-  // *** Set up UI states ***
   $stateProvider
-
-  // Home page
   .state('home', {
     url: '/',
     templateUrl: 'src/menuapp/templates/home.template.html'
   })
+  .state('categoryList', {
+    url: '/category-list',
+    templateUrl: 'src/menuapp/templates/category-list.template.html',
+    controller: 'CategoryListController as cateLstCtrl',
+    resolve: {
+      items: ['MenuDataService', function (MenuDataService) {
+        return MenuDataService.getAllCategories();
+      }]
+    }
+  })
+  .state('items', {
+    url: '/items/{category}',
+    templateUrl: 'src/menuapp/templates/items.template.html',
+    controller: 'ItemsController as itemsCtrl',
+    resolve: {
+      items: ['MenuDataService', '$stateParams', function (MenuDataService, $stateParams) {
+        return MenuDataService.getItemsForCategory($stateParams.category);
+      }]
+    }
+  });
 
-  // Premade list page
-  // .state('mainList', {
-  //   url: '/main-list',
-  //   templateUrl: 'src/shoppinglist/templates/main-shoppinglist.template.html',
-  //   controller: 'MainShoppingListController as mainList'
-  // });
+
 }
 
 })();
